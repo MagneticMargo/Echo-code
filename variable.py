6d345a69e9f332fe8df5610fe25d512364d14be2
MagneticMargo <n11301207@gmail.com>
2025-07-02 00:36:10 +0530

    Add files via upload
    day-1 added variable.py (basic python variable eg)

---
+++ variable.py
@@ -0,0 +1,43 @@
+# variable = a container for a value 
+
+#string : alwys written in double/singel qoute mark
+first_name ="magnetic"
+food = "south pole"
+email = "magnetic123@margo.com"
+
+print(f"hello {first_name}")
+print(f"you like to eat : {food}")
+print(f"your email is : {email}")
+
+#inteegrs : have whole numbers
+age = 25
+quantity = 4
+num_of_student = 100
+
+print(f"you are {age} yeras old")
+print(f"you have bought {quantity} apples")
+print(f"total students in a class : {num_of_student}")
+
+#float : it contain decimal portion
+price = 10.99
+gpa = 8.9
+distance = 3.4
+
+print(f"one apple costs : {price}")
+print(f"your gpa is :{gpa}")
+print(f"the dist from school to home is {distance} km")
+
+#boolen : have two value i.e : True either False , mostly used in if-else statements 
+is_student = True
+print (f"you are a student : {is_student}")
+
+
+
+user_name = "crow"
+year = 2025
+pi =3.14
+is_admin = True
+print (F"the user is : {user_name}")
+print(f"it is :{year}")
+print(f"value of pi is : {pi}")
+print(F"admin : {is_admin}")
